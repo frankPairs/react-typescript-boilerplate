966fe96e8cf428346dd562628107c81b5c28aa47
Frank Parejo <francisco.parejo.lopez@gmail.com>
2021-06-08 22:26:13 +0200

    add base styles.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import { render } from 'react-dom';
 
 import App from './App';
+import './styles/variables.css';
+import './styles/normalize.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const el = document.getElementById('root');
